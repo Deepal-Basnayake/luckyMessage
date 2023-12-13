b0421745841558a61e89e91b314d900dcac63067
Deepal <deepal.basnayake@yahoo.com>
2023-12-13 16:26:54 +0000

    adding final results

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-//get a random number 
-let randomNumber = Math.floor(Math.random() * 5)
-console.log(randomNumber)
-
-//adding messages list
+function randomMessage() {
+    var randomWish = ['You are ok','You are good','You are very well','You are not bad'];
+    var msg = document.querySelector('p');
+    msg.innerHTML = randomWish[Math.floor(Math.random() * randomWish.length)];
+}
